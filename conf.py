86c548381b83b34370d1907d264970a53cc66a87
christoph albrecht <christoph.albrecht@oxid-esales.com>
2023-11-27 09:50:09 +0100

    Version switch adjusted: OXDEV-7292 -- DE

--- conf.py
+++ conf.py
@@ -138,7 +138,7 @@ html_context = {
         [
          ('1.2', 'https://docs.oxid-esales.com/modules/paypal-checkout/de/1.2/'),
          ('1.3', 'https://docs.oxid-esales.com/modules/paypal-checkout/de/1.3/'),
-         ('2.3', 'https://docs.oxid-esales.com/modules/paypal-checkout/de/2.3/'),
+
          ],
     'languages':
        [('DE', 'https://docs.oxid-esales.com/modules/paypal-checkout/de/1.3/'),
