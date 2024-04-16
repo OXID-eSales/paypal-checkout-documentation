8eaf7fd826ca486c4176059cb3681f0b43ade050
christoph albrecht <christoph.albrecht@oxid-esales.com>
2024-04-16 11:49:46 +0200

    240 version switch adjusted -- EN

--- conf.py
+++ conf.py
@@ -135,10 +135,9 @@ html_theme = 'sphinx_rtd_theme'
 html_context = {
     'current_version': '2.2',
     'versions':
-        [
-         ('2.2', 'https://docs.oxid-esales.com/modules/paypal-checkout/en/2.2/'),
+        [('2.2', 'https://docs.oxid-esales.com/modules/paypal-checkout/en/2.2/'),
          ('2.3', 'https://docs.oxid-esales.com/modules/paypal-checkout/en/2.3/'),
-
+         ('2.4', 'https://docs.oxid-esales.com/modules/paypal-checkout/en/2.4/'),
          ],
     'languages':
        [('DE', 'https://docs.oxid-esales.com/modules/paypal-checkout/de/2.2/'),
