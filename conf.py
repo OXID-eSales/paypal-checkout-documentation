3356980af3837e5d1b0d8fa83f53b386ea5eb061
christoph albrecht <christoph.albrecht@oxid-esales.com>
2024-08-22 15:44:39 +0200

    OXDEV-8288: V. 2.5 adjust version switch

--- conf.py
+++ conf.py
@@ -134,7 +134,6 @@ html_theme = 'sphinx_rtd_theme'
 
 html_context = {
     'current_version': '2.3',
-    'current_version': '2.2',
     'versions62': [
          ('1.2', 'https://docs.oxid-esales.com/modules/paypal-checkout/en/1.2/'),
          ('1.3', 'https://docs.oxid-esales.com/modules/paypal-checkout/en/1.3/'),
